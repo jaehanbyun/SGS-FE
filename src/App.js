5781778dc14dbba365e361f05f15c249aebf218d
kjh302903 <kjh302903@gmail.com>
2023-03-25 15:04:31 +0900

    style: 폰트 최적화
    영어 400,700 Roboto, 한글 400,700 NotoSans 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Preview from "./pages/Preview";
+import "./styles/default.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/>
-          
+          <Route path="/" element={<Login />} />
+          <Route path="/preview" element={<Preview />} />
         </Routes>
       </BrowserRouter>
     </div>
