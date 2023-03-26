36439dcef9cc35d39462c28984a96196b780c445
kjh302903 <kjh302903@gmail.com>
2023-03-26 15:04:37 +0900

    fix: import App.css되어 오류나던것 수정
    App.js에 import App.css가 되어 오류 나던것 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Login from "./pages/Login";
 import Preview from "./pages/Preview";
 import "./styles/default.css";
