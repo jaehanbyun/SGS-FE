b85313880beaf3c43f6dfa33e2bd0665811414e2
kjh302903 <kjh302903@gmail.com>
2023-04-09 11:13:31 +0900

    refactor: CalendarModal 위치 변경 및 css 수정
    CalendarModal의 위치를 Modal 폴더안으로 수정 및 css 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import Calendar from "./components/CalendarModal/Calendar";
 import SideBar from "./components/SideBar";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
@@ -23,7 +22,6 @@ function App() {
           <Route path="/" element={<Preview />} />
           <Route path="/login" element={<Login />} />
           <Route path="/main" element={<Main />} />
-          <Route path="/calendar" element={<Calendar />} />
         </Routes>
       </BrowserRouter>
     </div>
