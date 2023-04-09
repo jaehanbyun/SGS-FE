0896674046aa4170bdcd3fa55fe6eadae7445ea7
kjh302903 <kjh302903@gmail.com>
2023-04-09 17:44:09 +0900

    refactor: CalendarModal 위치 재변경
    CalendarModal 위치 변경이 안되어 재변경

---
+++ src/components/Modal/ProfileModal/index.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from "react";
+import styles from "./ProfileModal.module.css";
+
+const ProfileModal = () => {
+  const ref = useRef();
+  const onClick = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.modalWrapper}>
+        <div className={styles.modal} ref={ref}>
+          <div className={styles.top}>
+            <p>프로필</p>
+            <img src="images/exit.svg" alt="exit" onClick={onClick} />
+          </div>
+          <div className={`${styles.imageWrapper} ${styles.content}`}>
+            <div className={styles.image}>
+              <img src="images/account_circle.svg" alt="profileImage" />
+            </div>
+          </div>
+          <div className={`${styles.nickname} ${styles.content}`}>
+            <p>닉네임</p>
+            <p>홍길동</p>
+          </div>
+          <div className={`${styles.email} ${styles.content}`}>
+            <p>이메일</p>
+            <p>abcd1234@gmail.com</p>
+          </div>
+          <div className={`${styles.site} ${styles.content}`}>
+            <p>웹사이트</p>
+            <p>https://github.com/</p>
+          </div>
+          <div className={`${styles.introduce} ${styles.content}`}>
+            <p>소개</p>
+            <pre>
+              안녕하세요.
+              <br />
+              부산대학교 재학생입니다.
+            </pre>
+          </div>
+          <div className={styles.bottom}>
+            <h5>평균 공부 시간</h5>
+            <p>03:10:05</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileModal;
