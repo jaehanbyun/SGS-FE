1389966d5b8758ea6a95f12727befe48494dfc60
kjh302903 <kjh302903@gmail.com>
2023-04-27 14:43:11 +0900

    feat: selectedUserInfo 리듀서 추가
    selectedUserInfo 리듀서 추가

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import selectedChannelReducer from "./selectedChannel/slice";
 import selectedProfileIconReducer from "./selectedProfileIcon/slice";
 import selectedUserStateReducer from "./selectedUserState/slice";
+import selectedUserInfoReducer from "./selectedUserInfo/slice";
 
 const store = configureStore({
   reducer: {
     selectedChannel: selectedChannelReducer.reducer,
     selectedProfileIcon: selectedProfileIconReducer.reducer,
     selectedUserState: selectedUserStateReducer.reducer,
+    selectedUserInfo: selectedUserInfoReducer.reducer,
   },
 });
 
