99fc922956ecaa6712ac7273c28da55f14f22bb6
kjh302903 <kjh302903@gmail.com>
2023-05-29 15:54:30 +0900

    feat: 로그인페이지, 찾기페이지 파싱 추가
    로그인 페이지와 찾기 페이지 파싱기능 추가
    isFindPage 참 >> 찾기 페이지
    	   거짓 >> 로그인 페이지
    isFindId 참 >> id 찾기
    	 거짓 >> pw 찾기

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import Signin from "../../components/Login/Signin";
 import Signup from "../../components/Login/Signup";
 import styles from "./Login.module.css";
+import FindId from "../../components/Login/FindId";
+import FindPw from "../../components/Login/FindPw";
 
 export default function Login() {
   const [isLoginPage, setIsLoginPage] = useState(true);
+  const [isFindPage, setIsFindPage] = useState(false);
+  const [isFindId, setIsFindId] = useState(false);
   const toggleSign = (e) => {
     e.preventDefault();
     setIsLoginPage(!isLoginPage);
@@ -14,10 +18,27 @@ export default function Login() {
       <div className={`${styles.form} ${styles.login}`}>
         <div className={styles.formContent}>
           <header className={styles.header}>
-            {isLoginPage ? "로그인" : "회원가입"}
+            {isFindPage
+              ? isFindId
+                ? "아이디 찾기"
+                : "비밀번호 찾기"
+              : isLoginPage
+              ? "로그인"
+              : "회원가입"}
           </header>
-          {isLoginPage ? (
-            <Signin isLoginPage={isLoginPage} toggleSign={toggleSign} />
+          {isFindPage ? (
+            isFindId ? (
+              <FindId setIsFindPage={setIsFindPage} />
+            ) : (
+              <FindPw setIsFindPage={setIsFindPage} />
+            )
+          ) : isLoginPage ? (
+            <Signin
+              setIsFindId={setIsFindId}
+              setIsFindPage={setIsFindPage}
+              isLoginPage={isLoginPage}
+              toggleSign={toggleSign}
+            />
           ) : (
             <Signup isLoginPage={isLoginPage} toggleSign={toggleSign} />
           )}
