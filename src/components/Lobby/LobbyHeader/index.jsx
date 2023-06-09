3cd45f15a30200f7ad70a775bfd34d660c53dd1c
kjh302903 <kjh302903@gmail.com>
2023-06-09 13:01:38 +0900

    feat: 방목록 새로고침 버튼 생성
    방 목록 새로고침하는 버튼 생성

--- src/components/Lobby/LobbyHeader/index.jsx
+++ src/components/Lobby/LobbyHeader/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./LobbyHeader.module.css";
-const LobbyHeader = () => {
+const LobbyHeader = ({ scrollRef, setRefresh, setIsScroll, setIsData }) => {
   return (
     <div className={styles.header}>
       <div className={styles.item}>
         <p>채팅방 이름</p>
+      </div>
+      <div className={styles.refresh}>
+        <img
+          onClick={() => {
+            scrollRef.current.scrollTo(0, 0);
+            console.log(scrollRef.current);
+            setRefresh((prev) => !prev);
+            setIsScroll(false);
+            setIsData(true);
+          }}
+          src="/images/refresh.svg"
+          alt="refresh"
+        />
       </div>
       <div className={styles.item}>
         <p>입장 인원</p>
