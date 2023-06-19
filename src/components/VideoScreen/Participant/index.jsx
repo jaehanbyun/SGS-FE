d3f9f7d522e46928f758a5060385657afd550942
YunSukHyun <czcz9207@gmail.com>
2023-06-19 22:12:09 +0900

    feat: 카메라 눌러서 메뉴 띄우기

--- src/components/VideoScreen/Participant/index.jsx
+++ src/components/VideoScreen/Participant/index.jsx
@@ -1,20 +1,39 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./participant.module.css";
 
-const Participant = ({ participant, tmp }) => {
+const Participant = ({ displayMenu, setDisplayMenu, participant, tmp }) => {
   const vidRef = useRef();
+  const [xy, setXY] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     vidRef.current.srcObject =
-      participant[1].type === "local"
-        ? participant[1].rtcPeer.getLocalStream()
-        : participant[1].rtcPeer.getRemoteStream();
+      participant.type === "local"
+        ? participant.rtcPeer.getLocalStream()
+        : participant.rtcPeer.getRemoteStream();
   }, [participant, tmp]);
+  const openMenu = (e) => {
+    setXY({ x: e.clientX, y: e.clientY });
+    setDisplayMenu(true);
+  };
   return (
-    <li>
-      <video className={styles.video} ref={vidRef} autoPlay playsInline />
-      <div className={styles.name}>{participant[1].type}</div>
-      <button onClick={() => console.log(participant)}>participant</button>
-    </li>
+    <>
+      <li onClick={openMenu}>
+        <video className={styles.video} ref={vidRef} autoPlay playsInline />
+        <div className={styles.name}>{participant.type}</div>
+        <button onClick={() => console.log(participant)}>participant</button>
+      </li>
+      {displayMenu && (
+        <ul
+          className={styles.menuList}
+          style={{ top: `${xy.y + 10}px`, left: `${xy.x + 5}px` }}
+        >
+          <li>경고</li>
+          <li>강퇴</li>
+          <li>방장 위임</li>
+          <li>프로필 보기</li>
+        </ul>
+      )}
+    </>
   );
 };
 
