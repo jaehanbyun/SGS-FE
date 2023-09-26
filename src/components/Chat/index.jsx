90b443c34d238c2af080d5d33305638e015016cd
YunSukHyun <czcz9207@gmail.com>
2023-09-26 17:20:22 +0900

    css: drawler for screen size under 1200px

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -5,10 +5,25 @@ import Members from "./Members";
 
 const Chat = ({ participants, roomId, chatList, setChatList }) => {
   return (
-    <div className={styles.container}>
-      <Members participants={participants} />
-      <ChatArea roomId={roomId} chatList={chatList} setChatList={setChatList} />
-    </div>
+    <>
+      <input
+        className={styles.chatHiddenCheck}
+        type="checkbox"
+        id="chatToggle"
+      />
+      <label className={styles.chatCheck} htmlFor="chatToggle">
+        <img src="/images/chat_drawler.svg" alt="menu" />
+      </label>
+      <div className={styles.chat}>
+        <Members participants={participants} />
+        <ChatArea
+          roomId={roomId}
+          chatList={chatList}
+          setChatList={setChatList}
+        />
+      </div>
+      <div className={styles.obfuscator}></div>
+    </>
   );
 };
 
